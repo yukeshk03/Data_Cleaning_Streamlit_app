d3f702d672a4cca61c5cf399f0502c0b47d75a51
Yukesh K <65612359+yukeshk03@users.noreply.github.com>
2025-01-08 10:19:12 +0530

    Update data_cleaning_web_app.py

--- data_cleaning_web_app.py
+++ data_cleaning_web_app.py
@@ -98,27 +98,31 @@ else:
                 st.write(f'Rows: {shape[0]}')
                 st.write(summary(df))
 
-        # Column name modifications
-        st.write('### *Column Name Changes*')
+     
         with st.expander("Change Column Names", expanded=True):
-            before_col = st.session_state.modified_columns
-            before_col_df = pd.DataFrame(before_col, columns=['Column Name'])
-            st.table(before_col_df)
-            
-            col3, col4, col5, col6 = st.columns(4)
+        before_col = st.session_state.modified_columns
+        before_col_df = pd.DataFrame(before_col, columns=['Column Name'])
+        st.table(before_col_df)
+    
+        # Use st.columns to align buttons in the same row
+        col3, col4, col5, col6 = st.columns(4)
+        with col3:
             if st.button('Capital'):
                 st.session_state.modified_columns = [col.title() for col in before_col]
+        with col4:
             if st.button('Uppercase'):
                 st.session_state.modified_columns = [col.upper() for col in before_col]
+        with col5:
             if st.button('Lowercase'):
                 st.session_state.modified_columns = [col.lower() for col in before_col]
+        with col6:
             if st.button('Spaces ➡ Underscore'):
                 st.session_state.modified_columns = [col.replace(" ", "_") for col in before_col]
-            df.columns = st.session_state.modified_columns
-
-            st.success("Column name changes applied.")
-            st.table(pd.DataFrame(df.columns, columns=['Updated Column Names']))
-
+    
+        df.columns = st.session_state.modified_columns
+    
+        st.success("Column name changes applied.")
+        st.table(pd.DataFrame(df.columns, columns=['Updated Column Names']))
         # Rename column functionality
         st.write("### *Rename Column Names*")
         column_select = st.selectbox('Select column to rename', options=st.session_state.modified_columns)
